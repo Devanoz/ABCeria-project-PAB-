3808fbe4a67b3070708ce1e8d82a8edcc48e9676
syuhda <20523238@students.uii.ac.id>
2022-12-07 21:10:54 +0700

    mengerjakan halaman home (menambahkan empty activity di home, menambah android resource directory 'menu', dll. | bottom navigation masi belum bisa stay, carousel belum jadi dibuat)

--- app/src/main/java/com/example/abceria/HalamanHome.kt
+++ app/src/main/java/com/example/abceria/HalamanHome.kt
@@ -3,11 +3,9 @@ package com.example.abceria
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HalamanHome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_halaman_home)
     }
-
 }
