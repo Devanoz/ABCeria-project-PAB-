a0512332b14014548b4764e57b24f1da4511732f
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2022-12-10 01:12:21 +0700

    integrasi fitur login

--- app/src/main/java/com/example/abceria/Activity/home/HalamanHome.kt
+++ app/src/main/java/com/example/abceria/Activity/home/HalamanHome.kt
@@ -1,10 +1,13 @@
 package com.example.abceria.Activity.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.example.abceria.Activity.auth.Auth
+import com.example.abceria.Activity.settings.Settings
 import com.example.abceria.R
 import com.example.abceria.db.DB
 import com.example.abceria.model.user.User
@@ -17,13 +20,22 @@ class HalamanHome : AppCompatActivity() {
     private val fireStore = DB.getFirestoreInstance()
     private val currentUser = Auth.getAuthInstance().currentUser
 
+    //components
+    private lateinit var imvProfile: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_halaman_home)
         supportActionBar?.hide()
 
         tvUsername = findViewById(R.id.home_tv_username)
+        imvProfile = findViewById(R.id.home_imv_profile)
        setProfileUsername()
+
+        imvProfile.setOnClickListener{
+            val intent = Intent(this,Settings::class.java)
+            this.startActivity(intent)
+        }
     }
 
 
--- app/src/main/java/com/example/abceria/Activity/settings/Settings.kt
+++ app/src/main/java/com/example/abceria/Activity/settings/Settings.kt
@@ -1,12 +1,37 @@
 package com.example.abceria.Activity.settings
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.abceria.Activity.auth.Auth
+import com.example.abceria.Activity.auth.Login
 import com.example.abceria.R
 
 class Settings : AppCompatActivity() {
+
+    private val auth = Auth.getAuthInstance()
+    private lateinit var btnLogout: Button
+    private val currentUser = auth.currentUser
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        btnLogout = findViewById(R.id.settings_btn_logout)
+
+        btnLogout.setOnClickListener {
+            auth.signOut()
+        }
+
+        auth.addAuthStateListener {
+            if(currentUser == null){
+                val intent = Intent(this, Login::class.java)
+                startActivity(intent)
+            }
+
+        }
     }
+
 }
+
